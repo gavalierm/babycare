45687c0d0dae0a8fab584728210796095fc09004
gavalierm <marcel@gavalier.sk>
2024-12-07 02:38:48 +0100

    sworker

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
- 
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .catch(error => {
+                return new Response('Network error', {
+                    status: 408,
+                    headers: { 'Content-Type': 'text/plain' }
+                });
+            })
+    );
+}); 
